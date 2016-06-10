1e6b227f22834eb9cba70729f08014b145217532
Brian Tanner <brian@tanner.io>
2016-06-10 03:18:54 -0400

    remove sharding

--- index.js
+++ index.js
@@ -3,23 +3,9 @@
 const discordbot = require("discordbot-core");
 const path = require('path');
 const getenv = require('getenv');
-const cluster = require('cluster');
-const sleep = require('sleep').sleep;
-const os = require('os');
 
-if (cluster.isMaster) {
-  console.log("MASTER");
-  for (let i = 0; i < os.cpus().length; i++) {
-    cluster.fork({ shardId: i, shardCount: os.cpus().length });
-    sleep(5);
-  }
-} else {
-  console.log("WORKER", process.env.shardId, process.env.shardCount);
-  discordbot.start({
-    ytApiKey: getenv('YT_APIKEY', ''),
-    musicPath: path.join(__dirname, 'music'),
-    collections: ['playlists', 'songs'],
-    shardId: parseInt(process.env.shardId,10),
-    shardCount: parseInt(process.env.shardCount,10)
-  });
-}
+discordbot.start({
+  ytApiKey: getenv('YT_APIKEY', ''),
+  musicPath: path.join(__dirname, 'music'),
+  collections: ['playlists', 'songs']
+});
